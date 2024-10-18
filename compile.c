bd3c2381115d0478f0cde63682118f77e78bf1f2
Hide1046 <t236017@st.yamagata-u.ac.jp>
2024-10-18 19:17:34 +0900

    Add files via upload

---
+++ compile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int k,g,d;
+    printf("How many 何屋未来?");
+    scanf("%d", &k);
+    
+    printf("How many zaki?");
+	scanf("%d", &g);
+	d=k+g;
+    printf("The number of 麻雀人 is %d", d);
+
+    return 0;
+}
