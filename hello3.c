23ef7224ee6d04d5923e09d105bb2340b18d019a
Hide1046 <t236017@st.yamagata-u.ac.jp>
2024-11-01 19:09:39 +0900

    Add files via upload

---
+++ hello3.c
@@ -0,0 +1,15 @@
+#include<float.h>
+#include<limits.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("char型の最大値:%d\n", CHAR_MAX);
+    printf("char型の最小値:%d\n", CHAR_MIN);
+    printf("char型の符号なし最大値:%d\n", UCHAR_MAX);
+    printf("long型の最大値:%ld\n", LONG_MAX);
+    printf("long型の最小値:%ld\n", LONG_MIN);
+
+    return 0;
+}
